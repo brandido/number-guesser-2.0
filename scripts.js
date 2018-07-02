12c266fd98300bd5b43a3bfc2633997bdc1197a4
Brandon Cunningham <brandido@gmail.com>
2018-07-01 19:59:31 -0600

    Basic refactoring to clean code Adjusted line-height property on CSS to remove extra height on user number feedback Tested to make sure an error message displayed if the guess is not a number (e.g. parseInt() returns NaN).

--- scripts.js
+++ scripts.js
@@ -12,22 +12,14 @@ console.log(resultRandomNumber);
 
 // listening for a submit on the inputForm
 inputForm.addEventListener('submit', startGame);
+// listening for click on reset button
 resetButton.addEventListener('click', reloadPage);
-  // function(e){
-//   e.preventDefault(); 
-//   // taking the user input value, converting it to a number, assigning it to guessNum variable
-//   var guessNum = parseInt(userInputGuess.value);
-//   // display user's last guess
-//   showNumber.innerText = guessNum;
-//   // calling the guessFeedback function and passing it arguments of guessNum and resultRandomNumber
-//   guessFeedback(guessNum, resultRandomNumber);
-// });
 
 function startGame(e) {
 e.preventDefault(); 
   // taking the user input value, converting it to a number, assigning it to guessNum variable
   var guessNum = parseInt(userInputGuess.value);
-  // display user's last guess
+  // display user's last guess in HTML
   showNumber.innerText = guessNum;
   // calling the guessFeedback function and passing it arguments of guessNum and resultRandomNumber
   guessFeedback(guessNum, resultRandomNumber);
@@ -40,20 +32,19 @@ function randomNumber() {
  function guessFeedback(guess, randomNum) {
   if (guess > randomNum) {
     showGuessFeedback.innerText = 'That is too high';
-    console.log('too high');
   } else if (guess < randomNum) {
     showGuessFeedback.innerText = 'That is too low';
-    console.log('too low');
   } else {
     showGuessFeedback.innerText = 'BOOM!';
-    console.log('boom');
   }
 };
-//use reload
+//use reload method
 function reloadPage(e) {
   e.preventDefault();
   location.reload();
 }
 
-// Listen for event on input form, if there enable clear button
+// Listen for event (onchange attribute)on input form, if there enable clear button
+// use change event with 
+// Or -- Use the validity.valid in JS with :valid or :invalid CSS pseudo-class property
 userInputGuess.addEventListener()
